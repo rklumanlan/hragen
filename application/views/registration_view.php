a0bd80e631e8cfe6b2463c0a751b0cc4e8eb478a
rklumanlan <rklumanlan@gmail.com>
2015-03-06 15:45:29 +0800

    remove username field

--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -11,7 +11,7 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2" aria-expanded="false">
-                
+
                  <?php echo form_open("user/login",'class="navbar-form navbar-right"'); ?>
                     <div class="form-group">
                         <input type="text" id="email" name="email" class="form-control" value="" placeholder="Email" />
@@ -21,7 +21,7 @@
 				    </div>
                     <input type="submit"  class="form-control" value="Sign in" />
                 <?php echo form_close(); ?>
-                
+
             </div>
             <!-- /.navbar-collapse -->
         </div>
@@ -32,21 +32,13 @@
 
         <!-- Jumbotron Header -->
         <header class="jumbotron hero-spacer col-xs-12 col-sm-12 col-md-12 col-lg-12  ">
-        
-        <div class="reg_form col-xs-10 col-sm-10 
+
+        <div class="reg_form col-xs-10 col-sm-10
         col-md-10 col-lg-8 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
         <div class="form_title">Sign Up</div>
         <div class="form_sub_title">It's free and anyone can join</div>
         <?php echo form_open("user/registration",'id="regform"'); ?>
             <div class="form-horizontal">
-                <div class="form-group">
-                    <label for="user_name" class="control-label col-sm-4 ">User Name:</label>
-                    <div class="col-sm-8">
-                    	<input type="text" id="user_name" name="user_name" class="form-control"
-                         value="<?php echo set_value('user_name'); ?>" />
-                         <?php echo "<span class='error'>".form_error('user_name')."</span>"; ?>
-                    </div>
-                </div>
                 <div class="form-group">
                     <label for="email_address" class="control-label col-sm-4 ">Your Email:</label>
                     <div class="col-sm-8">
@@ -78,28 +70,12 @@
                 </div>
             <?php echo form_close(); ?>
         </div>
-        
-        
-        
-        
-        
+
+
+
+
+
         </div>
 	</div>
-            
+
 </header>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
